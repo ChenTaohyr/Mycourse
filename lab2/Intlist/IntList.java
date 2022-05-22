b5eb43d451b415c448fc8a5d6e19649785a5e56b
‘ct’ <adgrtgs1434144675@qq.com>
2022-05-23 02:19:06 +0800

    Complete lab2_modified

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -104,6 +104,10 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if(A==null){
+            return B;
+        }
+
         IntList C = new IntList(A.first,null);
         IntList D=C;
         A=A.rest;
